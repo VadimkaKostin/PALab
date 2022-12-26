8f2026f656e73e97af6bb8388620a01b748ac5da
VadimKostin <vadkostinxm@gmail.com>
2022-12-26 12:13:58 +0200

    Edit Program.cs

--- Lab4/Lab4/Lab4/Program.cs
+++ Lab4/Lab4/Lab4/Program.cs
@@ -7,9 +7,15 @@ namespace Lab4
         public static void Main()
         {
             //Створення графу із 300 вершинами
+            Console.WriteLine("Creating graph...");
             Graph graph = new Graph(300);
             graph.GenerateGraphRandomly(30);
+            Console.WriteLine("Graph is created!");
 
+            Console.Write("\nPress Enter to start iterations: ");
+            Console.ReadLine();
+
+            Console.WriteLine("\nIteration Started.");
             //Ітерація із знаходженням значення цільової функції
             int iteration = 0;
             int bestCromatickNumber = int.MaxValue;
